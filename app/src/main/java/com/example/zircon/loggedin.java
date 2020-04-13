63c78dcef9138bdbe66f60d7e6dcd6a2681a18f0
Ehtesham <ehtesham_1234@live.com>
2020-04-13 19:09:57 +0530

    Changes Made to Logged in Activity

--- app/src/main/java/com/example/zircon/loggedin.java
+++ app/src/main/java/com/example/zircon/loggedin.java
@@ -1,32 +1,44 @@
 package com.example.zircon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
-public class loggedin extends AppCompatActivity implements View.OnClickListener {
-private TextView textView;
+public class loggedin extends AppCompatActivity {
+
+private Toolbar toolbar;
+private ViewPager viewPager;
+private TabLayout tabLayout;
+private TabAdapter tabAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loggedin);
-        textView = findViewById(R.id.textView);
-        textView.setText(ParseUser.getCurrentUser().getUsername().toString() + ", Welcome . Click Me to Logout");
+        setTitle("Zicron");
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        viewPager = findViewById(R.id.viewpager);
+        tabLayout = findViewById(R.id.tablayout);
+
+
+
+        tabAdapter = new TabAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(tabAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
     }
 
-    @Override
-    public void onClick(View view) {
-        ParseUser.logOutInBackground(new LogOutCallback() {
-            @Override
-            public void done(ParseException e) {
-                finish();
-            }
-        });
-    }
+
 }
